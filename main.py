f4e9413e271b6407b1eecaaec775f3e32bb98b54
김정석 <144312310+KimJenog-Seok@users.noreply.github.com>
2025-09-11 13:21:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 # === 환경변수에서 자격 증명 읽기 ===
