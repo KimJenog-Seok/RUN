2b0a97a9b92f3953a13f708c607a749f2f0cfa24
김정석 <144312310+KimJenog-Seok@users.noreply.github.com>
2025-09-30 15:02:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ WORKSHEET_NAME = "홈쇼핑TOP100"
 # 유틸
 # ------------------------------------------------------------
 def make_driver():
-    """GitHub Actions/서버/로컬 공용 크롬 드라이버 (Headless)."""
+# GitHub Actions/서버/로컬 공용 크롬 드라이버 (Headless).
     opts = webdriver.ChromeOptions()
     opts.add_argument("--headless=new")
     opts.add_argument("--no-sandbox")
